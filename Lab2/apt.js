190c0050192a596ea2796024d63adcdc66bc3d13
cderamos <charm.deramos@gmail.com>
2020-03-02 01:11:48 -0500

    Error messages/update messages for homepage done

--- Lab2/apt.js
+++ Lab2/apt.js
@@ -16,10 +16,30 @@ function checkEmptyFields() {
       //refresh form
     }
 }
-function tester(){
-  alert("WORKS");
-}
 
-function updateAPTInfo(){
-  
+function submitSuccess(){
+
+  if(document.getElementById("aname1").value == "" && document.getElementById('aid1').value == "" &&
+     document.getElementById('atype1').value == "" && document.getElementById('aversion1').value == "") {
+    alert("Nothing updated.");
+  } else {
+    alert("Success. APT info updated.")
+    var d = new Date();
+    document.getElementById('moddate').innerHTML= d.toUTCString();
+    //update info
+    var link = document.getElementById("aname1").value;
+    if(link != ""){
+      document.getElementById('attack1').textContent = link;
+    }
+    if(document.getElementById('aid1').value != ""){
+      document.getElementById('id1').innerHTML = document.getElementById('aid1').value;
+    }
+    if(document.getElementById('atype1').value !=""){
+      document.getElementById('type1').innerHTML = document.getElementById('atype1').value;
+    }
+    if(document.getElementById('aversion1').value !=""){
+      document.getElementById('version1').innerHTML = document.getElementById('aversion1').value;
+    }
+  }
+  document.getElementById("form1").reset();
 }

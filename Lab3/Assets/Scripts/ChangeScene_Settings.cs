bb4b5b8b63aca33792d7a2eaa755c36356dfee96
cderamos <charm.deramos@gmail.com>
2020-03-10 16:14:54 -0400

    Added a settings screen

---
+++ Lab3/Assets/Scripts/ChangeScene_Settings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene_Settings : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("SettingsScene");
+    }
+}
